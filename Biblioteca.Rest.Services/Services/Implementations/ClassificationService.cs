2ac1eec720f500e8eaab61e62b4efdb17179bfaa
Yoab <kevin02lopez02@gmail.com>
2025-09-15 21:07:15 -0600

    Se agrega la interface y el service de Classification

---
+++ Biblioteca.Rest.Services/Services/Implementations/ClassificationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.Rest.Services.Services.Interfaces;
+using BibliotecaRest.Data.Data;
+
+namespace Biblioteca.Rest.Services.Services.Implementations
+{
+    public class ClassificationService : IClassificationService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClassificationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
