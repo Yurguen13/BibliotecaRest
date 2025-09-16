74354fe142d2be6f471721b2d8fee3b90f41e6c2
Yoab <kevin02lopez02@gmail.com>
2025-09-16 14:01:17 -0600

    Se actualizan los mensajes para Classifications

--- Biblioteca.Rest.Services/Constants/Messages.cs
+++ Biblioteca.Rest.Services/Constants/Messages.cs
@@ -11,15 +11,42 @@ namespace Biblioteca.Rest.Services.Constants
 
         public static class Success
         {
-            //Classification
+            //-----Create-----
+
+                //Classification 
             public const string ClassificationCreated = "Clasificacion agregada exitosamente";
+
+
+            //-----Update-----
+                //Classification
+            public const string ClassificationUpdated = "Clasificacion actualizada exitosamente";
+
+            //-----Delete-----
+                //Classification
+            public const string ClassificationDeleted = "Clasificacion eliminada exitosamente";
+
         }
 
         public static class Error
         {
-            //Classification
+            //-----Search-----
+
+                //Classification
             public const string ClassificationNotFoundWithId = "Clasificacion con ID {0} no encontrado";
             public const string ClassificationNotFound = "Clasificacion no encontrado";
+
+            //-----Create-----
+                //Classification
+            public const string ClassificationCreateError = "Hubo un error al crear la clasificacion";
+
+
+            //-----Update-----
+                //Classification
+            public const string ClassificationUpdateError = "Hubo un error al actualizar la clasificacion";
+
+            //-----Delete-----
+                //Classification
+            public const string ClassificationDeleteError = "Hubo un eror al eliminar la clasificacion";
         }
     }
 }
