b6038b494d8decba5b70cdccce2723f58b2339b1
Yoab <kevin02lopez02@gmail.com>
2025-09-15 19:23:08 -0600

    Se termina el contexto de la base de datos (ApplicationDbContext)

--- BibliotecaRest.Data/ApplicationDbContext.cs
+++ BibliotecaRest.Data/ApplicationDbContext.cs
@@ -7,11 +7,16 @@ namespace BibliotecaRest.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        public DbSet<Autor> Autors {  get; set; }
+        public DbSet<Author> Authors {  get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+        public DbSet<Books> Books { get; set; } //check the plural
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Classification> Classifications { get; set; }
+        public DbSet<Loans> Loans {  get; set; } //check the plural
         public DbSet<Publisher> Publishers { get; set; }
-        public DbSet<Reader> Readers { get; set; }
-        public DbSet<Registry> Registries {  get; set; }
+        public DbSet<Reader> Readers { get; set; } 
+        public DbSet<Classification> Classifications { get; set; }
+        public DbSet<Specimens> Specimens { get; set; }//check the plural
+
     }
 }
