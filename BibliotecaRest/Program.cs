468690bfe58b3a977dbf39335b36381bd2de794a
Yoab <kevin02lopez02@gmail.com>
2025-09-15 19:29:27 -0600

    Se agrega el contexto de la base de datos dentro de Program.cs

--- BibliotecaRest/Program.cs
+++ BibliotecaRest/Program.cs
@@ -1,6 +1,14 @@
+using BibliotecaRest.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Database context
+var connection = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidCastException("Connection string 'DefaultConnection' not found");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
