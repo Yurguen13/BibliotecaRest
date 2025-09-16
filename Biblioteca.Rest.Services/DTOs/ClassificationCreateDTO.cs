74b73baf58e7e32f3d62499cdd09a26130979977
Yoab <kevin02lopez02@gmail.com>
2025-09-15 20:48:14 -0600

    Se crean los DTO para Classification, tambien se agregan las carpetas de Implementations e Interfaces

---
+++ Biblioteca.Rest.Services/DTOs/ClassificationCreateDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Rest.Services.DTOs
+{
+    public class ClassificationCreateDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Description is required")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Code is required")]
+        public string Code { get; set; }
+    }
+}
