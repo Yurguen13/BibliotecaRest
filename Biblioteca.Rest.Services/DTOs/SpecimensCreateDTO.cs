c6e3af64e3228439829df14d8d8bd85bb9d6b70c
Yoab <kevin02lopez02@gmail.com>
2025-09-17 23:14:55 -0600

    Se agrega SpecimensCreateDTO

--- Biblioteca.Rest.Services/DTOs/SpecimensCreateDTO.cs
+++ Biblioteca.Rest.Services/DTOs/SpecimensCreateDTO.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Biblioteca.Rest.Services.DTOs
 {
-    public class SpecimensCreateDTO
+    public class SpecimensCreateDTO : RegistryDTO
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage="El precio es requerido")]
+        [Range(50, double.MaxValue, ErrorMessage ="El precio debe ser mayor a 50")]
         public Decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Se debe ingresar un libro")]
         public int BooksId { get; set; }
+
+        [Required(ErrorMessage ="La condicion del libro es requerida")]
         public string Condition { get; set; }
         public string Observations { get; set; }
-        public string Books { get; set; }
     }
 }
